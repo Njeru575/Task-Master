8d8d4839c57d282bf82b9d0ef931bddb5b420f38
Valerie <britneyvalerie85@gmail.com>
2025-04-09 23:03:17 +0300

    changes added

---
+++ frontend/src/components/projectDetails.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import TaskForm from "../components/TaskForm";
+
+const ProjectDetails = () => {
+  const { id } = useParams();
+  const [project, setProject] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/projects/${id}`)
+      .then((res) => res.json())
+      .then(setProject);
+
+    fetch(`http://localhost:5000/api/tasks?project_id=${id}`)
+      .then((res) => res.json())
+      .then(setTasks);
+  }, [id]);
+
+  const addTask = (newTask) => setTasks([...tasks, newTask]);
+
+  return project ? (
+    <div>
+      <h2>{project.title}</h2>
+      <p>{project.description}</p>
+
+      <h3>Tasks</h3>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.title} - {task.status}
+          </li>
+        ))}
+      </ul>
+
+      <h4>Add Task</h4>
+      <TaskForm projectId={project.id} onCreate={addTask} />
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
+};
+
+export default ProjectDetails;
